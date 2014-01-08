17b17a604ddd0c333fef4e694a1de3c4074c7851
Anthony+Alex <Anthony+Alex@appacademy.io>
2014-01-08 16:40:25 -0500

    added colorize

--- chess.rb
+++ chess.rb
@@ -1,6 +1,8 @@
+# encoding: UTF-8
 require './pieces.rb'
 require './board.rb'
 require './deep_dup.rb'
+require 'colorize'
 
 class Game
   KEY_MAP = {
@@ -13,6 +15,27 @@ class Game
     "7" => 1,
     "8" => 0
   }
+
+  WHITE_MAP = {
+    :K => "\u2654",
+    :Q => "\u2655",
+    :R => "\u2656",
+    :B => "\u2657",
+    :k => "\u2658",
+    :P => "\u2659",
+    :_ => "\u0020"
+  }
+
+  BLACK_MAP = {
+    :K => "\u265A",
+    :Q => "\u265B",
+    :R => "\u265C",
+    :B => "\u265D",
+    :k => "\u265E",
+    :P => "\u265F",
+    :_ => "\u0020"
+  }
+
   attr_reader :board
 
   def initialize
@@ -21,7 +44,7 @@ class Game
 
   def play
     color = :w
-    puts "Let's play chess!"
+    puts "Let's play chess!".colorize(:light_blue)
 
     loop do
       render
@@ -33,11 +56,11 @@ class Game
     end
 
     puts "Checkmate! #{color} lost!"
+    render
   end
 
   def take_turn(color)
     begin
-      puts "It's #{color}'s turn!"
       puts "Which piece would you like to move? (ex. b1)"
       start_input = gets.chomp.downcase.split("")
       start_move = [KEY_MAP[start_input.last],
@@ -57,8 +80,18 @@ class Game
 
   def render
     render = @board.draw_board
-    render.each do |row|
-      puts row.to_s
+    render.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        k = i + j
+        color = k.even? ? :yellow : :red
+        #cell = " " if cell == :_
+        if @board[[i,j]] && @board[[i,j]].color == :w
+          print " #{WHITE_MAP[cell]} ".colorize( :background => color)
+        else
+          print " #{BLACK_MAP[cell]} ".colorize( :background => color)
+        end
+      end
+      print "\n"
     end
     nil
   end
