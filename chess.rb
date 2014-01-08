8fe69dbb01ae07a0976fad9b254ddff695f4c7a4
Anthony+Alex <Anthony+Alex@appacademy.io>
2014-01-08 15:52:55 -0500

    added 1-8 a-h grid input

--- chess.rb
+++ chess.rb
@@ -3,6 +3,16 @@ require './board.rb'
 require './deep_dup.rb'
 
 class Game
+  KEY_MAP = {
+    "1" => 7,
+    "2" => 6,
+    "3" => 5,
+    "4" => 4,
+    "5" => 3,
+    "6" => 2,
+    "7" => 1,
+    "8" => 0
+  }
   attr_reader :board
 
   def initialize
@@ -27,10 +37,14 @@ class Game
 
   def take_turn(color)
     begin
-      puts "What piece would you like to move? (x,y)"
-      start_move = gets.chomp.split(",").map(&:to_i)
+      puts "What piece would you like to move? (ex. g1)"
+      start_input = gets.chomp.downcase.split("")
+      start_move = [KEY_MAP[start_input.last],
+                    start_input.first.ord - "a".ord]
       puts "Where would you like to move it?"
-      end_move = gets.chomp.split(",").map(&:to_i)
+      end_input = gets.chomp.downcase.split("")
+      end_move = [KEY_MAP[end_input.last],
+                    end_input.first.ord - "a".ord]
       @board.move(start_move, end_move, color)
     rescue
       retry
